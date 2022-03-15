4555587329cfe4f337c81d2fd546695ff171a67c
randal.goss <randal.s.goss@usace.army.mil>
2022-03-15 09:16:26 -0400

    switched to golang modules.  switched from depricated go.geo to twpayne/go-geom for convex hull

--- ConcaveHull.go
+++ ConcaveHull.go
@@ -1,57 +1,59 @@
 package ConcaveHull
 
 /**
-	Golang implementation of https://github.com/skipperkongen/jts-algorithm-pack/blob/master/src/org/geodelivery/jap/concavehull/SnapHull.java
-	which is a Java port of st_concavehull from Postgis 2.0
- */
+Golang implementation of https://github.com/skipperkongen/jts-algorithm-pack/blob/master/src/org/geodelivery/jap/concavehull/SnapHull.java
+which is a Java port of st_concavehull from Postgis 2.0
+*/
 
 import (
-	"sort"
-	"github.com/furstenheim/go-convex-hull-2d"
-	"sync"
-	"github.com/furstenheim/SimpleRTree"
 	"math"
-	"github.com/paulmach/go.geo"
-	"github.com/paulmach/go.geo/reducers"
+	"sort"
+	"sync"
+
+	"github.com/furstenheim/SimpleRTree"
+	go_convex_hull_2d "github.com/furstenheim/go-convex-hull-2d"
+	"github.com/twpayne/go-geom/xy"
 )
 
 const DEFAULT_SEGLENGTH = 0.001
+
 type concaver struct {
-	rtree * SimpleRTree.SimpleRTree
-	seglength float64
-	options *Options
+	rtree            *SimpleRTree.SimpleRTree
+	seglength        float64
+	options          *Options
 	closestPointsMem []closestPoint
-	searchItemsMem []searchItem
-	flatPointBuffer []float64
-	rtreePool *sync.Pool
+	searchItemsMem   []searchItem
+	flatPointBuffer  []float64
+	rtreePool        *sync.Pool
 }
 type Options struct {
-	Seglength float64
+	Seglength                   float64
 	EstimatedRatioConcaveConvex int // estimated ratio of number of points between concave and convex hull. Will be used to allocate
-	ConcaveHullPool *sync.Pool
+	ConcaveHullPool             *sync.Pool
 }
 
 type concaveHullPoolElement struct {
-	fpbMem []float64
+	fpbMem           []float64
 	closestPointsMem []closestPoint
-	searchItemsMem []searchItem
-	rtreePool *sync.Pool // This will be passed down to rtree
-	convexHullPool *sync.Pool // This will be passed down to convex hull
-	pointsCopy FlatPoints
+	searchItemsMem   []searchItem
+	rtreePool        *sync.Pool // This will be passed down to rtree
+	convexHullPool   *sync.Pool // This will be passed down to convex hull
+	pointsCopy       FlatPoints
 }
-func Compute (points FlatPoints) (concaveHull FlatPoints) {
+
+func Compute(points FlatPoints) (concaveHull FlatPoints) {
 	return ComputeWithOptions(points, nil)
 }
-func ComputeWithOptions (points FlatPoints, o *Options) (concaveHull FlatPoints) {
+func ComputeWithOptions(points FlatPoints, o *Options) (concaveHull FlatPoints) {
 	sort.Sort(lexSorter(points))
 	return ComputeFromSortedWithOptions(points, o)
 }
-func ComputeFromSorted (points FlatPoints) (concaveHull FlatPoints) {
+func ComputeFromSorted(points FlatPoints) (concaveHull FlatPoints) {
 	return ComputeFromSortedWithOptions(points, nil)
 }
 
 // Compute concave hull from sorted points. Points are expected to be sorted lexicographically by (x,y)
-func ComputeFromSortedWithOptions (points FlatPoints, o *Options) (concaveHull FlatPoints) {
+func ComputeFromSortedWithOptions(points FlatPoints, o *Options) (concaveHull FlatPoints) {
 	// Create a copy so that convex hull and index can modify the array in different ways
 	var pointsCopy FlatPoints
 	var rtreeOptions SimpleRTree.Options
@@ -67,7 +69,7 @@ func ComputeFromSortedWithOptions (points FlatPoints, o *Options) (concaveHull F
 			rtreePool = poolEl.rtreePool
 			convexHullPool = poolEl.convexHullPool
 		} else {
-			rtreePool = &sync.Pool{} // If we are given a pool we want to initiate caching
+			rtreePool = &sync.Pool{}      // If we are given a pool we want to initiate caching
 			convexHullPool = &sync.Pool{} // If we are given a pool we want to initiate caching
 		}
 
@@ -84,12 +86,12 @@ func ComputeFromSortedWithOptions (points FlatPoints, o *Options) (concaveHull F
 	var wg sync.WaitGroup
 	wg.Add(2)
 	// Convex hull
-	go func () {
+	go func() {
 		points = go_convex_hull_2d.NewFromSortedArrayWithOptions(points, go_convex_hull_2d.Options{Pool: convexHullPool}).(FlatPoints)
 		wg.Done()
 	}()
 
-	func () {
+	func() {
 		rtree.LoadSortedArray(SimpleRTree.FlatPoints(pointsCopy))
 		wg.Done()
 	}()
@@ -106,8 +108,8 @@ func ComputeFromSortedWithOptions (points FlatPoints, o *Options) (concaveHull F
 		c.flatPointBuffer = poolEl.fpbMem[0:0]
 
 	} else {
-		c.closestPointsMem = make([]closestPoint, 0 , 2)
-		c.searchItemsMem = make([]searchItem, 0 , 2)
+		c.closestPointsMem = make([]closestPoint, 0, 2)
+		c.searchItemsMem = make([]searchItem, 0, 2)
 		estimatedProportionConcave2Convex := 4
 		if c.options != nil && c.options.EstimatedRatioConcaveConvex != 0 {
 			estimatedProportionConcave2Convex = c.options.EstimatedRatioConcaveConvex
@@ -120,88 +122,91 @@ func ComputeFromSortedWithOptions (points FlatPoints, o *Options) (concaveHull F
 	if o != nil && o.ConcaveHullPool != nil {
 		o.ConcaveHullPool.Put(
 			&concaveHullPoolElement{
-				rtreePool: rtreePool,
-				convexHullPool: convexHullPool,
-				searchItemsMem: c.searchItemsMem,
+				rtreePool:        rtreePool,
+				convexHullPool:   convexHullPool,
+				searchItemsMem:   c.searchItemsMem,
 				closestPointsMem: c.closestPointsMem,
-				fpbMem: c.flatPointBuffer,
-				pointsCopy: pointsCopy,
+				fpbMem:           c.flatPointBuffer,
+				pointsCopy:       pointsCopy,
 			},
 		)
 	}
 	return result
 }
 
-func (c * concaver) computeFromSorted (convexHull FlatPoints) (concaveHull FlatPoints) {
+func (c *concaver) computeFromSorted(convexHull FlatPoints) (concaveHull FlatPoints) {
 	// degerated case
-	if (convexHull.Len() < 3) {
+	if convexHull.Len() < 3 {
 		return convexHull
 	}
 
 	x0, y0 := convexHull.Take(0)
 	concaveHullBuffer := c.flatPointBuffer
 	concaveHullBuffer = append(concaveHullBuffer, x0, y0)
-	for i := 0; i<convexHull.Len(); i++ {
+	for i := 0; i < convexHull.Len(); i++ {
 		x1, y1 := convexHull.Take(i)
 		var x2, y2 float64
-		if i == convexHull.Len() -1 {
+		if i == convexHull.Len()-1 {
 			x2, y2 = convexHull.Take(0)
 		} else {
 			x2, y2 = convexHull.Take(i + 1)
 		}
 		sideSplit := c.segmentize(x1, y1, x2, y2)
-		for _, p := range(sideSplit) {
+		for _, p := range sideSplit {
 			concaveHullBuffer = append(concaveHullBuffer, p.x, p.y)
 		}
 	}
 	concaveHull = make([]float64, 0, len(concaveHullBuffer))
 	concaveHull = append(concaveHull, concaveHullBuffer...)
-	path := reducers.DouglasPeucker(geo.NewPathFromFlatXYData(concaveHull), c.seglength)
-	// reused allocated array
-	concaveHull = concaveHull[0:0]
-	reducedPoints := path.Points()
-
-	for _, p := range(reducedPoints) {
-		concaveHull = append(concaveHull, p.Lng(), p.Lat())
-	}
+	indices := xy.SimplifyFlatCoords([]float64(concaveHull), c.seglength, 2)
+	concaveHull = newHull(concaveHull, indices)
 	return concaveHull
 }
 
+func newHull(hull []float64, indices []int) []float64 {
+	newhull := make([]float64, 2*len(indices))
+	for i, index := range indices {
+		newhull[i*2] = hull[index*2]
+		newhull[i*2+1] = hull[index*2+1]
+	}
+	return newhull
+}
+
 // Split side in small edges, for each edge find closest point. Remove duplicates
-func (c * concaver) segmentize (x1, y1, x2, y2 float64) (points []closestPoint) {
-	dist := math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+func (c *concaver) segmentize(x1, y1, x2, y2 float64) (points []closestPoint) {
+	dist := math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
 	nSegments := math.Ceil(dist / c.seglength)
 	factor := 1 / nSegments
 	vX := factor * (x2 - x1)
 	vY := factor * (y2 - y1)
 
-	closestPoints := c.closestPointsMem[0: 0]
+	closestPoints := c.closestPointsMem[0:0]
 	closestPoints = append(closestPoints, closestPoint{index: 0, x: x1, y: y1})
 	closestPoints = append(closestPoints, closestPoint{index: int(nSegments), x: x2, y: y2})
 
-	if (nSegments < 2) {
+	if nSegments < 2 {
 		return closestPoints[1:]
 	}
 
-	stack := c.searchItemsMem[0: 0]
+	stack := c.searchItemsMem[0:0]
 	stack = append(stack, searchItem{left: 0, right: int(nSegments), lastLeftIndex: 0, lastRightIndex: 1})
 	for len(stack) > 0 {
 		var item searchItem
 		item, stack = stack[len(stack)-1], stack[:len(stack)-1]
-		if item.right - item.left <= 1 {
+		if item.right-item.left <= 1 {
 			continue
 		}
 		index := (item.left + item.right) / 2
 		fIndex := float64(index)
-		currentX := x1 + vX * fIndex
-		currentY := y1 + vY * fIndex
+		currentX := x1 + vX*fIndex
+		currentY := y1 + vY*fIndex
 		lx := closestPoints[item.lastLeftIndex].x
 		ly := closestPoints[item.lastLeftIndex].y
 		rx := closestPoints[item.lastRightIndex].x
 		ry := closestPoints[item.lastRightIndex].y
 
-		d1 := (currentX - lx) * (currentX - lx) + (currentY - ly) * (currentY - ly)
-		d2 := (currentX - rx) * (currentX - rx) + (currentY - ry) * (currentY - ry)
+		d1 := (currentX-lx)*(currentX-lx) + (currentY-ly)*(currentY-ly)
+		d2 := (currentX-rx)*(currentX-rx) + (currentY-ry)*(currentY-ry)
 		x, y, _, found := c.rtree.FindNearestPointWithin(currentX, currentY, math.Min(d1, d2))
 		if !found {
 			continue
@@ -216,7 +221,7 @@ func (c * concaver) segmentize (x1, y1, x2, y2 float64) (points []closestPoint) 
 			stack = append(stack, searchItem{left: item.left, right: index, lastLeftIndex: item.lastLeftIndex, lastRightIndex: newResultIndex})
 			// alloc
 			stack = append(stack, searchItem{left: index, right: item.right, lastLeftIndex: newResultIndex, lastRightIndex: item.lastRightIndex})
-		} else if (isNewLeft) {
+		} else if isNewLeft {
 			stack = append(stack, searchItem{left: item.left, right: index, lastLeftIndex: item.lastLeftIndex, lastRightIndex: item.lastRightIndex})
 		} else {
 			// don't add point to closest points, but we need to keep looking on the right side
@@ -231,32 +236,27 @@ func (c * concaver) segmentize (x1, y1, x2, y2 float64) (points []closestPoint) 
 
 type closestPoint struct {
 	index int
-	x, y float64
+	x, y  float64
 }
 
 type searchItem struct {
 	left, right, lastLeftIndex, lastRightIndex int
-
 }
-
-
-
-
 
 type FlatPoints []float64
 
-func (fp FlatPoints) Len () int {
+func (fp FlatPoints) Len() int {
 	return len(fp) / 2
 }
 
-func (fp FlatPoints) Slice (i, j int) (go_convex_hull_2d.Interface) {
-	return fp[2 * i: 2 * j]
+func (fp FlatPoints) Slice(i, j int) go_convex_hull_2d.Interface {
+	return fp[2*i : 2*j]
 }
 
-func (fp FlatPoints) Swap (i, j int) {
-	fp[2 * i], fp[2 * i + 1], fp[2 * j], fp[2 * j + 1] = fp[2 * j], fp[2 * j + 1], fp[2 * i], fp[2 * i + 1]
+func (fp FlatPoints) Swap(i, j int) {
+	fp[2*i], fp[2*i+1], fp[2*j], fp[2*j+1] = fp[2*j], fp[2*j+1], fp[2*i], fp[2*i+1]
 }
 
 func (fp FlatPoints) Take(i int) (x1, y1 float64) {
-	return fp[2 * i], fp[2 * i +1]
+	return fp[2*i], fp[2*i+1]
 }
---
+++ go.mod
@@ -0,0 +1,14 @@
+module concave-hull
+
+go 1.16
+
+require (
+	github.com/d4l3k/messagediff v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1
+	github.com/furstenheim/SimpleRTree v0.0.0-20181124165850-f68d71203793
+	github.com/furstenheim/go-convex-hull-2d v0.0.0-20181121204724-08788ab09726
+	github.com/pmezard/go-difflib v1.0.0
+	github.com/stretchr/testify v1.6.1
+	github.com/twpayne/go-geom v1.4.1
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0 // indirect
+)
